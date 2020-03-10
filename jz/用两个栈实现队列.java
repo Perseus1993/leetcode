fd403be8cad5a83c483e08505359a3bc79a6cc70
Perseus93 <1372643058@qq.com>
2020-03-10 20:09:10 +0800

    3-10update

---
+++ jz/用两个栈实现队列.java
@@ -0,0 +1,27 @@
+//解法1
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+
+        stack1.push(node);
+        System.out.println(stack1.peek());
+    }
+
+    public int pop() {
+        if(stack1.isEmpty() == true) return (Integer)null;
+        while(stack1.isEmpty() == false){
+            stack2.push(stack1.pop());
+
+        }
+        int res = stack2.pop();
+        while(stack2.isEmpty() == false){
+            stack1.push(stack2.pop());
+        }
+        return res;
+
+    }
+}
--- jz/第一个只出现一次的字符.java
+++ jz/第一个只出现一次的字符.java
@@ -1,4 +1,36 @@
 //解法1
+import java.util.HashMap;
+public class Solution {
+    public int FirstNotRepeatingChar(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++){
+        	char c = str.charAt(i);
+            map.put(c ,map.containsKey(c)? map.get(c) + 1 : 1);
+        }
+		for (int i = 0; i < str.length(); i++) {
+			if (map.get(str.charAt(i)) == 1) {
+				return i;
+			}
+		}
+		return -1;
+    }
+}
+//解法2
+public class Solution {
+    public int FirstNotRepeatingChar(String str) {
+        int[] arr = new int['z' + 1];
+        for(int i = 0; i < str.length(); i++){
+            arr[str.charAt(i)] += 1;
+        }
+        for(int i = 0; i < str.length(); i++){
+            if(arr[str.charAt(i)] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+//解法3
 public class Solution {
     public int FirstNotRepeatingChar(String str) {
         int[] arr = new int[58];
